a95abdd103687ca3b8f0639d40bb3c3078014473
Travis A. Dula <travisadula@gmail.com>
2021-04-01 16:10:32 -0500

    fixes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+# THERE IS NO PERMISSIONS CHECK
+This bot should be used with care or not at all, as it does not check if the calling member has any relevant permissions.
+
 # TrogBot
 Discord bot
--- bot.py
+++ bot.py
@@ -24,11 +24,12 @@ async def daniel(ctx):
         os.rename(f'resources/{ctx.guild}.json', f'resources/{ctx.guild}_{time.time()}.json')
     with open(f'resources/{ctx.guild}.json', 'w') as f:
         f.write(json.dumps(pairs))
+    await ctx.send('daniel complete')
 
 @bot.command()
-async def undaniel(ctx):
+async def undaniel(ctx, args=None):
     pairs = dict()
-    with open(f'resources/{ctx.guild}.json', 'r') as f:
+    with open(args if args else f'resources/{ctx.guild}.json', 'r') as f:
         pairs = json.loads(f.read())
     for user in ctx.guild.members:
         try:
@@ -36,10 +37,27 @@ async def undaniel(ctx):
                 await user.edit(nick=pairs[str(user.id)])
         except Exception as e:
             print(user, e)
+    await ctx.send('undaniel complete')
+
+@bot.command()
+async def danieltonormal(ctx):
+    await ctx.send('danieltonormal start')
+    for user in ctx.guild.members:
+        try:
+            if user.display_name == 'daniel':
+                await user.edit(nick=user.name)
+        except Exception as e:
+            ctx.send(f'Error on {user}: {e}')
+    await ctx.send('danieltonormal complete')
+
 
 @bot.command()
 async def hello(ctx):
     await ctx.send('Hello world!')
+
+@bot.command()
+async def messagehere(ctx, *, args):
+    await ctx.send(args)
 
 with open('resources/token', 'r') as f:
     bot.run(f.read())
